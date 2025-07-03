be7cf3d68a9c6f0f28c295d4b0aa1c6ee4c6c58c
LiaoAnn <1050123tom@gmail.com>
2025-07-03 14:28:06 +0000

    chore(excalidraw-frontend): remove cloudflare stuff

--- apps/excalidraw-frontend/vite.config.ts
+++ apps/excalidraw-frontend/vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    cloudflare({
-      inspectorPort: 9230,
-    }),
-  ],
+  plugins: [react()],
   server: {
     host: true, // Allow access from other devices on the network
   },
