ddfc6b053bdb16c754c6e4451b953619fdc1c5ae
LiaoAnn <1050123tom@gmail.com>
2025-07-05 17:19:09 +0000

    chore(excalidraw-frontend): Icons component

---
+++ apps/excalidraw-frontend/src/components/Icons.tsx
@@ -0,0 +1,61 @@
+import clsx from "clsx";
+type Opts = {
+  width?: number;
+  height?: number;
+  mirror?: true;
+} & React.SVGProps<SVGSVGElement>;
+
+// copy from excalidraw/packages/excalidraw/component/icons.tsx
+export const createIcon = (
+  d: string | React.ReactNode,
+  opts: number | Opts = 512
+) => {
+  const {
+    width = 512,
+    height = width,
+    mirror,
+    style,
+    ...rest
+  } = typeof opts === "number" ? ({ width: opts } as Opts) : opts;
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="img"
+      viewBox={`0 0 ${width} ${height}`}
+      className={clsx({ "rtl-mirror": mirror })}
+      style={style}
+      {...rest}
+    >
+      {typeof d === "string" ? <path fill="currentColor" d={d} /> : d}
+    </svg>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const tablerIconProps: Opts = {
+  width: 24,
+  height: 24,
+  fill: "none",
+  strokeWidth: 2,
+  stroke: "currentColor",
+  strokeLinecap: "round",
+  strokeLinejoin: "round",
+} as const;
+
+const modifiedTablerIconProps: Opts = {
+  width: 20,
+  height: 20,
+  fill: "none",
+  stroke: "currentColor",
+  strokeLinecap: "round",
+  strokeLinejoin: "round",
+} as const;
+
+// -----------------------------------------------------------------------------
+
+// tabler: arrow-left
+export const ArrowLeftIcon = createIcon(
+  <path stroke-width="1.25" d="M5 12h14M5 12l6 6m-6-6l6-6"></path>,
+  modifiedTablerIconProps
+);
