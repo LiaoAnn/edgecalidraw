4cf9cc8a05474b7644bdc4fd1868ef74b4b22b1e
LiaoAnn <1050123tom@gmail.com>
2025-07-05 04:12:02 +0000

    chore(excalidraw-frontend): dev proxy

--- apps/excalidraw-frontend/vite.config.ts
+++ apps/excalidraw-frontend/vite.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Allow access from other devices on the network
+    proxy: {
+      "/api": {
+        target: "http://localhost:8787", // Cloudflare Workers dev server
+        changeOrigin: true,
+      },
+    },
   },
 });
